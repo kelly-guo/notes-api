f0ad09894059adafd0b011a7d1f301fc181acaaf
Kelly <kellyguo1101@gmail.com>
2026-04-17 17:50:32 -0700

    added sharign entities

--- src/main/java/com/kelly/notesapi/controllers/NotificationController.java
+++ src/main/java/com/kelly/notesapi/controllers/NotificationController.java
@@ -3,6 +3,8 @@ package com.kelly.notesapi.controllers;
 import java.util.List;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -28,6 +30,16 @@ public class NotificationController {
     @GetMapping
     public List<NotificationResponse> getUnreadNotifications(@RequestParam Long userId){
         return notificationService.getUnreadNotifs(userId).stream().map(notificationMapper::toDto).toList();
+    }
+
+    @PatchMapping(path = "/{notifId}/read")
+    public void markOneAsRead (@PathVariable Long notifId){
+        notificationService.markAsRead(notifId);
+    }
+
+    @PatchMapping(path = "/read-all")
+    public void markAllAsRead (@RequestParam Long userId){
+        notificationService.markAllAsRead(userId);
     }
 
 
